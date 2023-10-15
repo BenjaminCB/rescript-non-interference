3f71c8fda692ee060f17d00edfb5d1c98d2689b3
bcb <benjamincb@hotmail.dk>
2023-10-15 23:19:56 +0200

    add ast and working lsp

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for rescript-non-interference
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
--- src/AST.hs
+++ src/AST.hs
@@ -8,4 +8,35 @@ data Expr = N Int
           | Abs Variable LevelT Expr
           | Seq Expr Expr
           | Let Variable LevelT Expr
+          | BO BinOper Expr Expr
+          | Rec [(Label, Expr)]
+          | Proj Expr Label
+          | IfThenElse Expr Expr Expr
+          | IfThen Expr Expr
+          | While Expr Expr
+          | For Variable Expr Expr Expr
+          | Loc Location
+          | Ref Expr
+          | Deref Expr
+          | Assign Variable Expr
           deriving (Eq, Show)
+
+newtype Variable = V String
+                 deriving (Eq, Show)
+
+newtype Location = L Int
+                 deriving (Eq, Show)
+
+data Label = LabelS String
+           | LabelI Int
+           deriving (Eq, Show)
+
+data BinOper = Add | Sub | Mul | Div
+             deriving (Eq, Show)
+
+data LevelT = TInt Int
+            | TAbs LevelT LevelT
+            deriving (Eq, Show)
+
+instance Ord LevelT where
+    compare _ _ = undefined
