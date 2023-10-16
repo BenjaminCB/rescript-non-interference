6bca81a8001f3cfcbe953a8817a0e9bf670f408b
bcb <benjamincb@hotmail.dk>
2023-10-16 14:08:58 +0200

    make progs module

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,12 @@ module Main where
 
 import AST
 import TypeChecker
+import Progs
 import qualified Data.Map as M
-
-high :: Expr
-high = Let (V "h") (TInt 1) $ N 1
-
-low :: Expr
-low = Let (V "l") (TInt 0) (Var $ V "h")
-
-prog :: Expr
-prog = Seq high low
 
 main :: IO ()
 main = do
     print $ N 1
     print $ check M.empty (TInt 0) (N 1)
-    print prog
-    print $ check M.empty (TInt 0) prog
+    print bindLowToHigh
+    print $ check M.empty (TInt 0) bindLowToHigh
---
+++ src/Progs.hs
@@ -0,0 +1,9 @@
+module Progs (bindLowToHigh) where
+
+import AST
+
+bindLowToHigh :: Expr
+bindLowToHigh = Seq high low
+  where
+    high = Let (V "h") (TInt 1) $ N 1
+    low = Let (V "l") (TInt 0) (Var $ V "h")
