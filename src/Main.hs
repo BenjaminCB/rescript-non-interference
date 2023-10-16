0776c7b14216292d2d1e9b7f038691eb1a0d636a
bcb <benjamincb@hotmail.dk>
2023-10-16 12:33:52 +0200

    make test prog

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,18 @@ import AST
 import TypeChecker
 import qualified Data.Map as M
 
+high :: Expr
+high = Let (V "h") (TInt 1) $ N 1
+
+low :: Expr
+low = Let (V "l") (TInt 0) (Var $ V "h")
+
+prog :: Expr
+prog = Seq high low
+
 main :: IO ()
 main = do
     print $ N 1
     print $ check M.empty (TInt 0) (N 1)
+    print prog
+    print $ check M.empty (TInt 0) prog
