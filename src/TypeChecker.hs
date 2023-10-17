7b66bad5c92296657baf70d6cddc02a59ee2af4b
bcb <benjamincb@hotmail.dk>
2023-10-17 12:46:04 +0200

    add more helpful error messages

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -25,7 +25,7 @@ check env pc expr = case expr of
     Unit -> Right (env, TInt 0)
     (Var x) -> do
         l <- elookup x env
-        sat (l >= pc) "error"
+        sat (l >= pc) "Var: l < pc"
         return (env, l)
     (BO _ e1 e2) -> do
         (_, l1) <- check env pc e1
@@ -37,57 +37,57 @@ check env pc expr = case expr of
         (_, l2) <- check env pc' e2
         (_, l3) <- check env pc' e3
         let l = max l2 l3
-        sat (pc <= l1) "error"
-        sat (pc' <= l2) "error"
-        sat (pc' <= l3) "error"
+        sat (pc <= l1) "IfThenElse: pc > l1"
+        sat (pc' <= l2) "IfThenElse: pc' > l2"
+        sat (pc' <= l3) "IfThenElse: pc' > l3"
         return (env, l)
     (IfThen e1 e2) -> do
         (_, l1) <- check env pc e1
         let pc' = max l1 pc
         (_, l2) <- check env pc' e2
-        sat (pc <= l1) "error"
-        sat (pc' <= l2) "error"
+        sat (pc <= l1) "IfThen: pc > l1"
+        sat (pc' <= l2) "IfThen: pc' > l2"
         return (env, l2)
     (While e1 e2) -> do
         (_, l1) <- check env pc e1
         let pc' = max l1 pc
         (_, l2) <- check env pc' e2
-        sat (pc <= l1) "error"
-        sat (pc' <= l2) "error"
+        sat (pc <= l1) "While: pc > l1"
+        sat (pc' <= l2) "While: pc' > l2"
         return (env, max l1 l2)
     (For x e1 e2 e3) -> do
         (_, l1) <- check env pc e1
         (_, l2) <- check env pc e2
         let pc' = maximum [l1, l2, pc]
         (_, l3) <- check (M.insert x pc' env) pc' e3 -- TODO check this
-        sat (pc <= l1) "error"
-        sat (pc <= l2) "error"
-        sat (pc' <= l3) "error"
+        sat (pc <= l1) "For: pc > l1"
+        sat (pc <= l2) "For: pc > l2"
+        sat (pc' <= l3) "For: pc' > l3"
         return (env, maximum [l1, l2, l3])
     (Let x l e) -> do
         (_, l') <- check env pc e
-        sat (l >= l') "error"
-        sat (l >= pc) "error"
+        sat (l >= l') "Let: l < l'"
+        sat (l >= pc) "Let: l < pc"
         return (M.insert x l env, l)
     (Seq e1 e2) -> do
         (env1, l1) <- check env pc e1
         (env2, l2) <- check env1 pc e2
-        sat (l1 >= pc) "error"
-        sat (l2 >= pc) "error"
+        sat (l1 >= pc) "Seq: l1 < pc"
+        sat (l2 >= pc) "Seq: l2 < pc"
         return (env2, max l1 l2)
     (Abs x l@(TAbs l1 l2) e) -> do
         (_, l') <- check (M.insert x l1 env) pc e
-        sat (l2 == l') "error"
+        sat (l2 == l') "Abs: l2 /= l'"
         return (env, l)
-    (Abs {}) -> Left "error"
+    (Abs {}) -> Left "Abs: not a function type"
     (App e1 e2) -> do
         (_, l1) <- check env pc e1
         (_, l2) <- check env pc e2
         case l1 of
             (TAbs l1' l2') -> do
-                sat (l1' == l2) "error"
+                sat (l1' == l2) "App: l1' /= l2"
                 return (env, l2')
-            _ -> Left "error"
+            _ -> Left "App: not a function type"
     (Rec fs) -> do
         ls <- traverse (fmap snd . check env pc . snd) fs
         return (env, maximum ls)
@@ -104,6 +104,6 @@ check env pc expr = case expr of
     (Assign x e) -> do
         (_, l') <- check env pc e
         l <- elookup x env
-        sat (l == l') "error"
-        sat (l >= pc) "error"
+        sat (l == l') "Assign: l /= l'"
+        sat (l >= pc) "Assign: l < pc"
         return (env, l)
