9f863ce37c398060055e3a5504e18fbcabbd0091
bcb <benjamincb@hotmail.dk>
2023-10-25 14:35:38 +0200

    find lowest level assign in abs

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -26,8 +26,25 @@ sat False e = do
 modifyFst :: MonadState (t, b) m => (t -> t) -> m ()
 modifyFst = modify . first
 
+getFst :: MonadState (t, b) m => m t
+getFst = gets fst
+
+putFst :: MonadState (t, b) m => t -> m ()
+putFst t = do
+    (_, b) <- get
+    put (t, b)
+
+
 modifySnd :: MonadState (a, t) m => (t -> t) -> m ()
 modifySnd = modify . second
+
+getSnd :: MonadState (a, t) m => m t
+getSnd = gets snd
+
+putSnd :: MonadState (a, t) m => t -> m ()
+putSnd t = do
+    (a, _) <- get
+    put (a, t)
 
 
 check :: Env -> LevelT -> Expr -> StateEither ([String], LevelT) (Env, LevelT)
@@ -47,45 +64,45 @@ check env pc expr = case expr of
         return (env, l)
     (BO _ e1 e2) -> do
         modifyFst (++ ["BO: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l1) <- check env pc e1
-        put trace
+        putFst trace
         (_, l2) <- check env pc e2
         return (env, max l1 l2)
     (IfThenElse e1 e2 e3) -> do
         modifyFst (++ ["IfThenElse: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l1) <- check env pc e1
         let pc' = max l1 pc
-        put trace
+        putFst trace
         (_, l2) <- check env pc' e2
-        put trace
+        putFst trace
         (_, l3) <- check env pc' e3
         return (env, maximum [l1, l2, l3])
     (IfThen e1 e2) -> do
         modifyFst (++ ["IfThen: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l1) <- check env pc e1
         let pc' = max l1 pc
-        put trace
+        putFst trace
         (_, l2) <- check env pc' e2
         return (env, max l1 l2)
     (While e1 e2) -> do
         modifyFst (++ ["While: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l1) <- check env pc e1
         let pc' = max l1 pc
-        put trace
+        putFst trace
         (_, l2) <- check env pc' e2
         return (env, max l1 l2)
     (For x e1 e2 e3) -> do
         modifyFst (++ ["For: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l1) <- check env pc e1
-        put trace
+        putFst trace
         (_, l2) <- check env pc e2
         let pc' = maximum [l1, l2, pc]
-        put trace
+        putFst trace
         (_, l3) <- check (M.insert x pc' env) pc' e3 -- TODO check this
         return (env, maximum [l1, l2, l3])
     (Let x l@(TInt _) e) -> do
@@ -102,24 +119,27 @@ check env pc expr = case expr of
         return (M.insert x l env, l)
     (Seq e1 e2) -> do
         modifyFst (++ ["Seq: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (env1, l1) <- check env pc e1
-        put trace
+        putFst trace
         (env2, l2) <- check env1 pc e2
         return (env2, max l1 l2)
     (Abs x l e) -> do
         modifyFst (++ ["Abs: " ++ show expr])
+        putSnd $ TInt 1000
         (_, l') <- check (M.insert x l env) pc e
-        return (env, TAbs (TInt 0) l l')
+        pc' <- getSnd
+        return (env, TAbs pc' l l')
     (App e1 e2) -> do
         modifyFst (++ ["App: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l1) <- check env pc e1
-        put trace
+        putFst trace
         (_, l2) <- check env pc e2
-        put trace
+        putFst trace
         case l1 of
-            (TAbs _ l1' l2') -> do
+            (TAbs pc' l1' l2') -> do
+                sat (pc' >= pc) "NotSat: pc' >= pc"
                 sat (l1' == l2) "NotSat: l1' == l2"
                 return (env, l2')
             _ -> fail "App: not a function type"
@@ -144,10 +164,11 @@ check env pc expr = case expr of
         return (env, l)
     (Assign x e) -> do
         modifyFst (++ ["Assign: " ++ show expr])
-        trace <- get
+        trace <- getFst
         (_, l') <- check env pc e
-        put trace
+        putFst trace
         l <- elookup x env
+        modifySnd (min l)
         sat (l >= l') "NotSat: l >= l'"
         sat (l >= pc) "NotSat: l >= pc"
         return (env, l)
