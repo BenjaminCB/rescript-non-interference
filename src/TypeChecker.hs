307d775ac0c49b5501d84f8e94a28bba65d2e26c
bcb <benjamincb@hotmail.dk>
2023-12-07 15:33:01 +0100

    add trace helper function

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -31,13 +31,18 @@ sat False e = do
     modify (++ [e])
     fail e
 
+trace :: String -> StateEither [String] a -> StateEither [String] a
+trace s m = do
+    t <- get
+    put $ t ++ [s]
+    a <- m
+    put t
+    return a
+
 check :: Env -> LevelT -> Expr -> StateEither [String] (Env, LevelT, LevelT)
 check env pc expr = case expr of
-    (LetInf x e) -> do
-        modify (++ ["LetInf: " ++ show expr])
-        trace <- get
+    (LetInf x e) -> trace ("LetInf: " ++ show expr) $ do
         (_, l, eff) <- check env pc e
-        put trace
         case l of
             t1 :-> (t2 :@ eff') -> do
                 sat (pc == Low) "NotSat: pc == Low"
@@ -46,80 +51,54 @@ check env pc expr = case expr of
                 sat (t1 `elem` [Low, High]) "NotSat: t1 `elem` [Low, High]"
                 sat (pc <= t1) "NotSat: pc <= t1"
                 return (M.insert x t1 env, Low, max t1 eff)
-    (Let x t1 e) -> do
-        modify (++ ["LetTInt: " ++ show expr])
-        trace <- get
+    (Let x t1 e) -> trace ("Let: " ++ show expr) $ do
         (_, t2, t3) <- check env pc e
-        put trace
         sat (t1 `elem` [Low, High]) "NotSat: t1 `elem` [Low, High]"
         sat (t2 `elem` [Low, High]) "NotSat: t2 `elem` [Low, High]"
         sat (t2 <= t1) "NotSat: t2 <= t1"
         sat (pc <= t1) "NotSat: pc <= t1"
         return (M.insert x t1 env, Low, min t1 t3)
-    (IfThenElse e1 e2 e3) -> do
-        modify (++ ["IfThenElse: " ++ show expr])
-        trace <- get
+    (IfThenElse e1 e2 e3) -> trace ("IfThenElse: " ++ show expr) $ do
         (_, l1, eff1) <- check env pc e1
-        put trace
         sat (l1 `elem` [Low, High]) "NotSat: l1 `elem` [Low, High]"
         let pc' = max l1 pc
         (_, l2, eff2) <- check env pc' e2
-        put trace
         (_, l3, eff3) <- check env pc' e3
         return (env, maximum [l1, l2, l3], minimum [eff1, eff2, eff3])
-    (Seq e1 e2) -> do
-        modify (++ ["Seq: " ++ show expr])
-        trace <- get
+    (Seq e1 e2) -> trace ("Seq: " ++ show expr) $ do
         (env1, _, eff1) <- check env pc e1
-        put trace
         (env2, l2, eff2) <- check env1 pc e2
         return (env2, l2, min eff1 eff2)
-    (While e1 e2) -> do
-        modify (++ ["While: " ++ show expr])
-        trace <- get
+    (While e1 e2) -> trace ("While: " ++ show expr) $ do
         (_, l1, eff1) <- check env pc e1
-        put trace
         sat (l1 `elem` [Low, High]) "NotSat: l1 `elem` [Low, High]"
         let pc' = max l1 pc
         (_, _, eff2) <- check env pc' e2
         return (env, Low, min eff1 eff2)
-    (For x e1 e2 e3) -> do
-        modify (++ ["For: " ++ show expr])
-        trace <- get
+    (For x e1 e2 e3) -> trace ("For: " ++ show expr) $ do
         (_, l1, eff1) <- check env pc e1
-        put trace
         sat (l1 `elem` [Low, High]) "NotSat: l1 `elem` [Low, High]"
         (_, l2, eff2) <- check env pc e2
-        put trace
         sat (l2 `elem` [Low, High]) "NotSat: l2 `elem` [Low, High]"
         let pc' = maximum [l1, l2, pc]
         (_, _, eff3) <- check (M.insert x pc' env) pc' e3
         return (env, Low, minimum [eff1, eff2, eff3])
-    (Var x) -> do
-        modify (++ ["Var: " ++ show expr])
+    (Var x) -> trace ("Var: " ++ show expr) $ do
         l <- elookup x env
         return (env, l, Empty)
-    (B _) -> do
-        modify (++ ["Bool: " ++ show expr])
+    (B _) -> trace ("Bool: " ++ show expr) $ do
         return (env, Low, Empty)
-    (N _) -> do
-        modify (++ ["Num: " ++ show expr])
+    (N _) -> trace ("Num: " ++ show expr) $ do
         return (env, Low, Empty)
-    Unit -> do
-        modify (++ ["Unit: " ++ show expr])
+    Unit -> trace ("Unit: " ++ show expr) $ do
         return (env, Low, Empty)
-    (Abs x l e) -> do
-        modify (++ ["Abs: " ++ show expr])
+    (Abs x l e) -> trace ("Abs: " ++ show expr) $ do
         (_, l', eff') <- check (M.insert x l env) pc e
         sat (pc <= eff') "NotSat: pc <= eff'"
         return (env, l :-> (l' :@ eff'), eff')
-    (App e1 e2) -> do
-        modify (++ ["App: " ++ show expr])
-        trace <- get
+    (App e1 e2) -> trace ("App: " ++ show expr) $ do
         (_, l1, eff1) <- check env pc e1
-        put trace
         (_, l2, eff2) <- check env pc e2
-        put trace
         case l1 of
             (l1' :-> (l2' :@ eff3)) -> do
                 let eff = minimum [eff1, eff2, eff3]
@@ -127,13 +106,9 @@ check env pc expr = case expr of
                 sat (l1' == l2) "NotSat: l1' == l2"
                 return (env, l2', eff)
             t -> fail $ "App: not a function type: " ++ show t
-    (BO _ e1 e2) -> do
-        modify (++ ["BO: " ++ show expr])
-        trace <- get
+    (BO _ e1 e2) -> trace ("BO: " ++ show expr) $ do
         (_, l1, eff1) <- check env pc e1
-        put trace
         (_, l2, eff2) <- check env pc e2
-        put trace
         sat (l1 `elem` [Low, High]) "NotSat: l1 `elem` [Low, High]"
         sat (l2 `elem` [Low, High]) "NotSat: l2 `elem` [Low, High]"
         return (env, max l1 l2, min eff1 eff2)
