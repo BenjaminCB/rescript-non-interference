ed7a265e3ae8b4a2e90e055249c8cbc1bddc8e3a
bcb <benjamincb@hotmail.dk>
2023-12-08 13:11:12 +0100

    add type checking to main

--- src/Main.hs
+++ src/Main.hs
@@ -11,30 +11,21 @@ main :: IO ()
 main = do
     (path:_) <- getArgs
     contents <- readFile path
-    putStrLn "File Content: "
+    putStrLn "File Content:"
     mapM_ putStrLn $ lines contents
-    putStrLn "Parsed Content: "
+    putStrLn "Parsed Content:"
     case parseInput contents of
         Left err -> print err
         Right e -> do
             print e
-
-checkLevel :: String -> Expr -> Int -> IO ()
-checkLevel name e n = do
-    print name
-    print e
-    putStrLn $ "Initial program counter: " ++ show n
-    let res = runStateEither (check M.empty Low e) []
-    case res of
-        Left (err, trace) -> do
-            putStr $ "Error: " ++ show err
-            putStrLn ""
-            putStrLn "Trace:"
-            mapM_ (putStrLn . ("    " ++)) trace
-        Right (t, _) -> do
-            putStrLn "State:"
-            print t
-    putStrLn ""
-
-checkDefault :: String -> Expr -> IO ()
-checkDefault name e = checkLevel name e 0
+            putStrLn "Type checking:"
+            let res = runStateEither (check M.empty Low e) []
+            case res of
+                Left (err, trace) -> do
+                    putStr $ "Error: " ++ show err
+                    putStrLn ""
+                    putStrLn "Trace:"
+                    mapM_ (putStrLn . ("    " ++)) trace
+                Right (t, _) -> do
+                    putStrLn "State:"
+                    print t
